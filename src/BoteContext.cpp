dd258d610148944116aa99a84e5cd5425ce6acfc
polistern <polistern@i2pmail.org>
2022-01-22 14:30:34 +0000

    chore: Updated copyright headers.

--- src/BoteContext.cpp
+++ src/BoteContext.cpp
@@ -1,5 +1,9 @@
 /**
  * Copyright (c) 2019-2022 polistern
+ *
+ * This file is part of pboted and licensed under BSD3
+ *
+ * See full license text in LICENSE file at top of project tree
  */
 
 #include <utility>
--- src/pboted.cpp
+++ src/pboted.cpp
@@ -1,22 +1,34 @@
 /**
- * Copyright (c) 2019-2021 polistern
+ * Copyright (c) 2019-2022 polistern
+ *
+ * This file is part of pboted and licensed under BSD3
+ *
+ * See full license text in LICENSE file at top of project tree
  */
 
 #include <cstdlib>
 
 #include "BoteDaemon.h"
 
-int main(int argc, char *argv[]) {
-  if (Daemon.init(argc, argv)) {
-    int res = Daemon.start();
-    if (res == 0) {
-      Daemon.run();
-    } else if (res > 0) {
-      return EXIT_SUCCESS;
-    } else {
-      return EXIT_FAILURE;
+int
+main (int argc, char *argv[])
+{
+  if (Daemon.init (argc, argv))
+    {
+      int res = Daemon.start ();
+      if (res == 0)
+        {
+          Daemon.run ();
+        }
+      else if (res > 0)
+        {
+          return EXIT_SUCCESS;
+        }
+      else
+        {
+          return EXIT_FAILURE;
+        }
+      Daemon.stop ();
     }
-    Daemon.stop();
-  }
   return EXIT_SUCCESS;
 }
