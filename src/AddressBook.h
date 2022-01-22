8238530ec626025a5e7c21d6dfb8729b98d715f0
polistern <polistern@i2pmail.org>
2022-01-22 12:02:28 +0000

    chore: Style fixes.

--- src/AddressBook.h
+++ src/AddressBook.h
@@ -1,50 +1,63 @@
 /**
- * Copyright (c) 2019-2021 polistern
+ * Copyright (c) 2019-2022 polistern
+ *
+ * This file is part of pboted and licensed under BSD3
+ *
+ * See full license text in LICENSE file at top of project tree
  */
 
-#ifndef ADDRESS_BOOK_H__
-#define ADDRESS_BOOK_H__
+#ifndef PBOTED_ADDRESS_BOOK_H__
+#define PBOTED_ADDRESS_BOOK_H__
 
 #include <map>
 #include <string>
 #include <vector>
 
-namespace pbote {
+namespace pbote
+{
 
-struct Contact {
+#define ADDRESS_BOOK_FILE_NAME "addressbook.txt"
+
+struct Contact
+{
 public:
-  Contact() = default;
+  Contact () = default;
 
   std::string alias{};
   std::string name{};
   std::string dest{};
 };
 
-class AddressBook {
+class AddressBook
+{
 public:
-  AddressBook();
-  AddressBook(std::string path, std::string pass);
-  ~AddressBook();
+  AddressBook ();
+  AddressBook (std::string path, std::string pass);
+  ~AddressBook ();
 
-  void load();
-  void save();
+  void load ();
+  void save ();
 
-  void add(std::string &alias, std::string &name, std::string &address);
-  bool name_exist(const std::string &name);
-  bool alias_exist(const std::string &alias);
-  std::string address_for_name(const std::string &name);
-  std::string address_for_alias(const std::string &alias);
-  void remove(const std::string &name);
+  void add (std::string &alias, std::string &name, std::string &address);
+  bool name_exist (const std::string &name);
+  bool alias_exist (const std::string &alias);
+  std::string address_for_name (const std::string &name);
+  std::string address_for_alias (const std::string &alias);
+  void remove (const std::string &name);
 
-  size_t size() { return contacts.size(); }
+  size_t
+  size ()
+  {
+    return contacts.size ();
+  }
 
-  //void setPassword();
-  //void changePassword();
-  //void encrypt();
-  //void decrypt();
+  // void setPassword();
+  // void changePassword();
+  // void encrypt();
+  // void decrypt();
 
 private:
-  std::vector<std::string> read();
+  std::vector<std::string> read ();
 
   std::string filePath_;
   std::string passwordHolder_;
@@ -54,4 +67,4 @@ private:
 
 } // namespace pbote
 
-#endif // ADDRESS_BOOK_H__
+#endif // PBOTED_ADDRESS_BOOK_H__
