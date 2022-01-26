d76b97d4d86e4a71d201760ddce736aea437c173
polistern <polistern@i2pmail.org>
2022-01-26 13:49:25 +0000

    fix: Fixed typo.

--- src/DHTworker.h
+++ src/DHTworker.h
@@ -111,7 +111,7 @@ struct Node : i2p::data::IdentityEx {
   std::string
   short_name ()
   {
-    return this.ToBase64 ().substr(0, 15), "...";
+    return this->ToBase64 ().substr(0, 15), "...";
   }
 
   void noResponse() {
