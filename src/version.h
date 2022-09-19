f8dfe4ee71684bdedf577967b03d2e3eac249a03
polistern <polistern@i2pmail.org>
2022-09-19 11:53:15 +0000

    chore: Updated version and debian changelog.

--- src/version.h
+++ src/version.h
@@ -16,7 +16,7 @@
 
 #define PBOTE_VERSION_MAJOR 0
 #define PBOTE_VERSION_MINOR 7
-#define PBOTE_VERSION_MICRO 10
+#define PBOTE_VERSION_MICRO 11
 
 #define PBOTE_VERSION                                                         \
   MAKE_VERSION (PBOTE_VERSION_MAJOR, PBOTE_VERSION_MINOR, PBOTE_VERSION_MICRO)
